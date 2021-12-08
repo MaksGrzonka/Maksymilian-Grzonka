3937ed363f63064225c623f4f1bacbbd00747c2d
MaksGrzonka <92087355+MaksGrzonka@users.noreply.github.com>
2021-12-08 22:32:16 +0100

    Add files via upload

---
+++ 3.2.0.3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+void swap(int& a, int& b)
+{
+    std::cout<< a << " " << b << "\n";
+    int c;
+    c=a; 
+    a=b;
+    b=c;
+    std::cout<< a << " " << b << "\n";
+}
+int main()
+{
+    int a = 42;
+    int b = 64;
+    swap(a,b);
+    return 0;
+}
